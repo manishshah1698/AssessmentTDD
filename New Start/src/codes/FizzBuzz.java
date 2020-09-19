481a5f42c3d3ce8de42a39fea337954b29b815fd
Shah Manish <manishshah1698@gmail.com>
2020-09-20 01:12:02 +0530

    Writing the main method and priting upto 100 in console using TDD method

--- New Start/src/codes/FizzBuzz.java
+++ New Start/src/codes/FizzBuzz.java
@@ -12,5 +12,14 @@ public class FizzBuzz {
 		else
 			return String.valueOf(i);
 	}
+	public static void main(String []args)
+	{
+		FizzBuzz f = new FizzBuzz();
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println(f.fiz(i));
+		}
+		
+	}
 	
 }
