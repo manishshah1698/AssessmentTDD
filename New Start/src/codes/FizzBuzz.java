f25e11aa22a97130aee34acf386199184fa9d132
Shah Manish <manishshah1698@gmail.com>
2020-09-20 01:01:36 +0530

    Writing test case for divisble by 3 and created method by using TDD

--- New Start/src/codes/FizzBuzz.java
+++ New Start/src/codes/FizzBuzz.java
@@ -1,29 +1,9 @@
 package codes;
 
 public class FizzBuzz {
-	public String fizzBuzz(int number)
-	{
-			return String.valueOf(number);
-	}
 
-	public String fizzBuzzDivisibleBy3(int i) {
-		if(i % 3 == 0)
-			return "Fizz";
-		else
-			return null;
+	public String fiz(int i) {
+		return "";
 	}
-
-	public String fizzBuzzDivisibleBy5(int i) {
-		if(i % 5 == 0)
-			return "Buzz";
-		else
-			return null;
-	}
-
-	public String fizzBuzzDivisibleBy15(int i) {
-		if(i % 15 == 0)
-			return "FizzBuzz";
-		else
-			return null;
-	}
+	
 }
--- New Start/src/codes/fizzBuzzTest.java
+++ New Start/src/codes/fizzBuzzTest.java
@@ -6,36 +6,14 @@ import org.junit.jupiter.api.Test;
 
 class fizzBuzzTest {
 
-	// Checking the number entered. 
-	@Test
-	public void testfor3() {
-	FizzBuzz fb = new FizzBuzz();
-	String result = fb.fizzBuzz(3);
-	assertEquals("3",result);
-	
-	}
 	
 	// Printing fizz for every multiple of 3.
 	@Test
 	public void testforDivisibleBy3() {
 	FizzBuzz fb = new FizzBuzz();
-	String result = fb.fizzBuzzDivisibleBy3(6);
+	String result = fb.fiz(6);
 	assertEquals("Fizz",result);
 	}
 
-	// Printing buzz for every multiple of 5.
-		@Test
-		public void testforDivisibleBy5() {
-		FizzBuzz fb = new FizzBuzz();
-		String result = fb.fizzBuzzDivisibleBy5(10);
-		assertEquals("Buzz",result);
-		}
-		
-		// Printing fizzbuzz for every multiple of 3 and 5.
-		@Test
-		public void testforDivisibleBy15() {
-		FizzBuzz fb = new FizzBuzz();
-		String result = fb.fizzBuzzDivisibleBy15(30);
-		assertEquals("FizzBuzz",result);	
-		}
+	
 }
