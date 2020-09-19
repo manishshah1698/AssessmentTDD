0805deee35a12e41ef6a01129216cd9fb7a14639
Shah Manish <manishshah1698@gmail.com>
2020-09-20 00:43:06 +0530

    Writing test case for Checking a number divisible by 3 and creating a method for it.

--- New Start/src/codes/FizzBuzz.java
+++ New Start/src/codes/FizzBuzz.java
@@ -5,4 +5,11 @@ public class FizzBuzz {
 	{
 			return String.valueOf(number);
 	}
+
+	public String fizzBuzzDivisibleBy3(int i) {
+		if(i % 3 == 0)
+			return "Fizz";
+		else
+			return null;
+	}
 }
--- New Start/src/codes/fizzBuzzTest.java
+++ New Start/src/codes/fizzBuzzTest.java
@@ -6,11 +6,21 @@ import org.junit.jupiter.api.Test;
 
 class fizzBuzzTest {
 
+	// Checking the number entered. 
 	@Test
 	public void testfor3() {
 	FizzBuzz fb = new FizzBuzz();
 	String result = fb.fizzBuzz(3);
 	assertEquals("3",result);
+	
+	}
+	
+	// Printing fizz for every multiple of 3.
+	@Test
+	public void testforDivisibleBy3() {
+	FizzBuzz fb = new FizzBuzz();
+	String result = fb.fizzBuzzDivisibleBy3(6);
+	assertEquals("Fizz",result);
 	
 	}
 
